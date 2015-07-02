6278fff178e38ad22254e81c432a0cb5a9b00a8d
Gustaf Forsslund <gustaf.forsslund@oktavilla.se>
2015-07-02 16:27:23 +0200

    Adds tests

--- test/modules/model-tests.js
+++ test/modules/model-tests.js
@@ -4,32 +4,148 @@ var Backbone = require("backbone");
 var BackboneDespotism = require("../../backbone-despotism.js");
 
 describe("Backbone Despotism", function() {
+  // Testing undefined properties
   it("Ignores undefined properties", function() {
     var MyStrictModel = Backbone.StrictModel.extend({
       props: {
-        "first": Backbone.StrictModel.type.STRING,
-        "last": Backbone.StrictModel.type.STRING
+        firstName: Backbone.StrictModel.type.STRING,
+        lastName: Backbone.StrictModel.type.STRING
       }
     });
 
-    var myStrictModelInstance = new MyStrictModel();
-    myStrictModelInstance.set("monkey", "banana"); // Should be ignored
-    myStrictModelInstance.set({
-      "first": "Gustaf",
-      "last": "Forsslund",
-      "animal": "monkey"
+    var model = new MyStrictModel();
+    model.set("fruit", "banana"); // Should be ignored
+    model.set({
+      firstName: "Bill",
+      lastName: "Drummond",
+      animal: "monkey"
     });
 
-    assert.deepEqual(myStrictModelInstance.attributes, {
-      "first": "Gustaf",
-      "last": "Forsslund"
+    assert.deepEqual(model.attributes, {
+      firstName: "Bill",
+      lastName: "Drummond"
     }, "Does not ignore undefined properties");
+  });
 
+  // Testing foreign keys
+  it("Uses foreign keys", function() {
+    var MyStrictModel = Backbone.StrictModel.extend({
+      props: {
+        firstName: Backbone.StrictModel.type.STRING,
+        lastName: {
+          type: Backbone.StrictModel.type.STRING,
+          foreignKey: "surname"
+        }
+      }
+    });
+    var model = new MyStrictModel();
+    model.set({
+      firstName: "James",
+      lastName: "Cauty"
+    });
+    assert.deepEqual(model.attributes, {
+      firstName: "James",
+      lastName: "Cauty"
+    }, "Uses key");
+    model.clear();
+    model.set({
+      firstName: "James",
+      surname: "Cauty"
+    });
+    assert.deepEqual(model.attributes, {
+      firstName: "James",
+      lastName: "Cauty"
+    }, "Uses foreign key");
+    model.clear();
+    model.set({
+      firstName: "James",
+      lastName: "Cauty",
+      surname: "Drummond"
+    });
+    assert.deepEqual(model.attributes, {
+      firstName: "James",
+      lastName: "Cauty"
+    }, "Prefers key to foreign key");
+  });
+
+  // Testing defaults
+  it("Uses default values", function() {
+    var MyStrictModel = Backbone.StrictModel.extend({
+      props: {
+        firstName: Backbone.StrictModel.type.STRING,
+        lastName: Backbone.StrictModel.type.STRING
+      },
+      defaults: {
+        firstName: "James"
+      }
+    });
+    var model = new MyStrictModel();
+    model.set("lastName", "Cauty");
+    assert.deepEqual(model.attributes, {
+      firstName: "James",
+      lastName: "Cauty"
+    }, "Uses default values");
+    model.clear();
+    model.set({
+      firstName: "Bill",
+      lastName: "Drummond"
+    });
+    assert.deepEqual(model.attributes, {
+      firstName: "Bill",
+      lastName: "Drummond"
+    }, "Ignores default value if value exists");
+  });
+  
+  // Testing defaults with foreign keys
+  it("Uses foreign keys with default values", function() {
+    var MyStrictModel = Backbone.StrictModel.extend({
+      props: {
+        firstName: Backbone.StrictModel.type.STRING,
+        lastName: {
+          type: Backbone.StrictModel.type.STRING,
+          foreignKey: "surname"
+        }
+      },
+      defaults: {
+        lastName: "Drummond"
+      }
+    });
+    var model = new MyStrictModel();
+    model.set({
+      firstName: "James",
+      lastName: "Cauty"
+    });
+    assert.deepEqual(model.attributes, {
+      firstName: "James",
+      lastName: "Cauty"
+    }, "Uses key");
+    model.clear();
+    model.set({
+      firstName: "James",
+      surname: "Cauty"
+    });
+    assert.deepEqual(model.attributes, {
+      firstName: "James",
+      lastName: "Cauty"
+    }, "Uses foreign key");
+    model.clear();
+    model.set({
+      firstName: "James",
+      lastName: "Cauty",
+      surname: "Timelord"
+    });
+    assert.deepEqual(model.attributes, {
+      firstName: "James",
+      lastName: "Cauty"
+    }, "Prefers key to foreign key");
+    model.clear();
+    model.set({
+      firstName: "James",
+      surname: "Cauty"
+    });
+    assert.deepEqual(model.attributes, {
+      firstName: "James",
+      lastName: "Cauty"
+    }, "Prefers foreign key to default");
   });
 });
-
-
-
-
-
-
